e0f339b81c16e99c5af3de09f1956d452a91bb3a
Elortex <mateuszhumeniuk@wp.pl>
2021-12-07 21:17:23 +0100

    Update day2p2.py
    addjusted code to get match the specifications of part 2

--- day2p2.py
+++ day2p2.py
@@ -1,12 +1,15 @@
 import sys
 posX = 0
 posY = 0
+aim = 0
 for command in sys.stdin:
     if command == '\n': break
     tempCommand = command.split()
-    if tempCommand[0] == 'down': posY += int(tempCommand[1])
-    elif tempCommand[0] == 'up': posY -= int(tempCommand[1])
-    else: posX += int(tempCommand[1])
+    if tempCommand[0] == 'down': aim += int(tempCommand[1])
+    elif tempCommand[0] == 'up': aim -= int(tempCommand[1])
+    else: 
+        posX += int(tempCommand[1])
+        posY += aim * int(tempCommand[1])
 
 print(posX)
 print(posY)
