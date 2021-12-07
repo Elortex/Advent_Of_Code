63c942d46fb1c0e44af44ba7bd49ceb45ed12c3c
Elortex <mateuszhumeniuk@wp.pl>
2021-12-07 20:12:53 +0100

    Update day1p2.py
    working "alpha" version of par2 of day1 task

--- day1p2.py
+++ day1p2.py
@@ -1,9 +1,19 @@
 import sys
+
 noOfIncrm = 0
-prevReading = int(sys.stdin.readline())
+tempList = []
+sumsList = []
+for i in range(3):
+    tempList.append(int(sys.stdin.readline()))
+sumsList.append(sum(tempList))
+
 for reading in sys.stdin:
     if reading == '\n': break
-    currReading = int(reading)
-    if currReading > prevReading: noOfIncrm += 1
-    prevReading = currReading
+    tempList.pop(0)
+    tempList.append(int(reading))
+    sumsList.append(sum(tempList))
+
+for i in range(1, len(sumsList)):
+    if sumsList[i] > sumsList[i-1]: noOfIncrm += 1
+
 print(noOfIncrm)
