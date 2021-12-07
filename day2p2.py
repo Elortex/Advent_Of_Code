dcb50540ddf261ecbbf8177349960fec9e96d88c
Elortex <mateuszhumeniuk@wp.pl>
2021-12-07 21:28:20 +0100

    Update day2p2.py
    last addjustments to day2 part2

--- day2p2.py
+++ day2p2.py
@@ -1,9 +1,11 @@
 import sys
+
 posX = 0
 posY = 0
 aim = 0
 for command in sys.stdin:
     if command == '\n': break
+    
     tempCommand = command.split()
     if tempCommand[0] == 'down': aim += int(tempCommand[1])
     elif tempCommand[0] == 'up': aim -= int(tempCommand[1])
